a07121e43eefbc4b209c56ef4884a332e38187da
Daniel La <la.danieln@gmail.com>
2026-03-18 14:59:38 -0400

    added: favoritebutton component. edits: backtotop title on button and gamecard to include favoritebutton

--- components/ui/GameCard.tsx
+++ components/ui/GameCard.tsx
@@ -2,27 +2,31 @@ import { RawgGame } from "@/types/rawg/game";
 import Image from "next/image";
 import Link from "next/link";
 import { StoreIcons } from "@/components/ui/StoreIcons";
+import { FavoriteButton } from "./FavoriteButton";
 
 type GameCardProps = {
     gameData: RawgGame
 }
 
-export const GameCard = ({ gameData }: GameCardProps ) => {
+export const GameCard = ({ gameData }: GameCardProps) => {
     const {
         slug,
         name,
         background_image,
         stores,
     } = gameData;
-    return(
-        <article className="bg-slate-900 border border-solid border-app-border rounded-lg h-full flex flex-col">
+    return (
+        <article className="bg-slate-900 border border-solid border-app-border rounded-lg h-full flex flex-col relative">
             {background_image && (
                 <div className="game-card-image-container aspect-video relative rounded-t-lg">
                     <Image src={background_image} alt="" fill className="object-cover rounded-t-lg"></Image>
                 </div>
             )}
             <div className="game-card-content p-5 h-full flex flex-col gap-2.5">
-                <StoreIcons stores={stores} />
+                <div className="flex flex-row justify-between items-center">
+                    <StoreIcons stores={stores} />
+                    <FavoriteButton name={name} slug={slug} />
+                </div>
                 <h3>{name}</h3>
                 <Link className="mt-auto button-outline" href={`/catalog/${slug}`} aria-label={`View details for ${name}`}>
                     View Game
