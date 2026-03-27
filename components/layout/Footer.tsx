261437af5e3a941d55fcc30787f612c7e397cd02
Daniel La <37962617+d-la@users.noreply.github.com>
2026-03-27 18:49:53 -0400

    remove: footer menus and about page (#9)

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -7,8 +7,8 @@ export const Footer = async () => {
 
     return (
         <footer className="footer bg-slate-900 w-full relative py-10 border-t border-solid border-app-border">
-            <div className="flex flex-col md:flex-row justify-start items-start max-w-7xl w-full mx-auto p-10 gap-10">
-                {footerMenus && footerMenus?.length > 0 && footerMenus?.map(menu => (
+            {footerMenus && footerMenus?.length > 0 && (<div className="flex flex-col md:flex-row justify-start items-start max-w-7xl w-full mx-auto p-10 gap-10">
+                { footerMenus?.map(menu => (
                     <div key={menu.id} className="footer-column">
                         <h4 className="footer-column-title pb-2 border-b border-solid border-app-border mb-2">{menu.name}</h4>
                         <ul className="footer-column-menu">
@@ -20,7 +20,7 @@ export const Footer = async () => {
                         </ul>
                     </div>
                 ))}
-            </div>
+            </div>)}
             <div className="footer-copyright text-center text-xs text-white">&copy; {year} Gaming Catalog. All rights reserved.</div>
         </footer>
     )
